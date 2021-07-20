2674bdfd27af8c372ee8a576e95c324ab9b819c6
thanhtv <tvthanhdl@gmail.com>
2021-07-20 11:46:11 +0700

    edit / delete functional

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.84.0">
-    <title>Signin Template · Bootstrap v5.0</title>
+    <title>CMS · Login</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sign-in/">
 
